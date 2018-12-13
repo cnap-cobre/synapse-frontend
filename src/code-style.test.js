4066cde290db98c362ba142b43afe03f7c582dc3
Kevin Dice <kevin.dice1@gmail.com>
2018-12-13 14:36:50 -0600

    Remove old useless tests - Will spin up some better ones soon

--- src/code-style.test.js
+++
@@ -1,6 +0,0 @@
-const recursive = require('recursive-readdir');
-const readFilePromise = require('fs-readfile-promise');
-
-test('indentation should be done with spaces, not tabs', () => recursive('./src', ['!*.js']).then(files => files.map(file => readFilePromise(file, 'utf8').then((contents) => {
-  expect(contents).not.toMatch(/\t/g);
-}))).then(promises => Promise.all(promises)));
--- src/components/WebShell/WebShell.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WebShell from './WebShell';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<WebShell />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
