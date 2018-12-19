5f4806e181fbd29e8a7843a6aa1a3d8324b3a4a1
Kevin Dice <kevin.dice1@gmail.com>
2018-12-19 13:07:37 -0600

    Lint errors

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -11,11 +11,12 @@ const Dashboard = () => (
           <Col xs={12}>
             <Card header={<h3 className="card-title">About</h3>} hr>
               <p>
-                Synapse is an open science gateway for the psychological sciences, tying together tools,
-                applications, and data in support of the research process.  The goal of the Synapse platform is
-                to aid researchers in conducting reproducible research.  Synapse is supported by the Cognitive
-                and Neurobiological Approaches to Plasticity (CNAP) Center of Biomedical Research Excellence
-                (COBRE) of the National Institutes of Health under grant number P20GM113109.
+                Synapse is an open science gateway for the psychological sciences, tying together
+                tools, applications, and data in support of the research process.  The goal of the
+                Synapse platform is to aid researchers in conducting reproducible research.
+                Synapse is supported by the Cognitive and Neurobiological Approaches to Plasticity
+                (CNAP) Center of Biomedical Research Excellence (COBRE) of the National Institutes
+                of Health under grant number P20GM113109.
               </p>
             </Card>
           </Col>
@@ -25,16 +26,15 @@ const Dashboard = () => (
           <Col md={8}>
             <Card header={<h4 className="card-title">Getting Started</h4>} hr>
               <p>
-                Synapse integrates with several services which you may already be using:  Beocat, Dropbox,
-                and JupyterHub, with support for Globus and Google Drive coming soon.
+                Synapse integrates with several services which you may already be using:
+                Beocat, Dropbox, and JupyterHub, with support for Globus and Google Drive
+                coming soon.
               </p>
             </Card>
           </Col>
 
           <Col md={4}>
-            <Card header={<h5 className="card-title">Link External Services</h5>} hr>
-
-            </Card>
+            <Card header={<h5 className="card-title">Link External Services</h5>} hr />
           </Col>
         </Row>
 
