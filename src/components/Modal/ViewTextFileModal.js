41221a8c01adb3e08a51b8bc4298a6686c9308ec
Kevin Dice <kevin.dice1@gmail.com>
2019-01-14 05:39:46 -0600

    Add a button to edit text files with jupyterhub

--- src/components/Modal/ViewTextFileModal.js
+++ src/components/Modal/ViewTextFileModal.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Modal from 'react-bootstrap/lib/Modal';
+import { Row, Col } from 'react-bootstrap';
 
 import { removeModal } from '../../store/ui/modals/Modals';
 import agaveFileActions from '../../services/Agave/agaveFileActions';
 import Loader from '../Loader/Loader';
 import dropboxFileActions from '../../services/Dropbox/dropboxFileActions';
 import type { ViewTextFileModalType } from '../../types/modalTypes';
+import { getJupyterHubUsername } from '../../store/userProfile/reducer';
+import jupytericon from '../SocialButtons/jupyterhub_icon.png'
 
 type Props = ViewTextFileModalType & {
   $removeModal(string): typeof undefined,
@@ -56,7 +59,7 @@ class ViewTextFileModal extends React.Component<Props, State> {
   };
 
   render() {
-    const { file } = this.props;
+    const { file, jupyterUsername } = this.props;
     const { show, contents } = this.state;
 
     return (
@@ -75,12 +78,30 @@ class ViewTextFileModal extends React.Component<Props, State> {
           { contents === '' ? (
             <Loader visible />
           ) : (
-            <pre style={{
-              overflow: 'auto', maxHeight: '80vh', wordWrap: 'normal', whiteSpace: 'pre',
-            }}
-            >
-              { contents }
-            </pre>
+              <>
+                <Row>
+                  <Col xs={12}>
+                <a className="btn btn-social socialaccount_provider btn-jupyterhub"
+                   href={`https://jupyterhub.beocat.ksu.edu/user/${jupyterUsername}/edit/${file.path.split('/').slice(3).join('/')}`}
+                   target="_blank"
+                   style={{
+                     float: 'right',
+                     marginBottom: '1em'
+                   }}
+                >
+                  <img src={jupytericon} />
+                  Edit in JupyterHub
+                </a>
+                  </Col>
+                </Row>
+
+                <pre style={{
+                  overflow: 'auto', maxHeight: '80vh', wordWrap: 'normal', whiteSpace: 'pre',
+                }}
+                >
+                  { contents }
+                </pre>
+              </>
           )}
         </Modal.Body>
 
@@ -94,6 +115,7 @@ class ViewTextFileModal extends React.Component<Props, State> {
 
 const mapStateToProps = store => ({
   csrftoken: store.csrf.token,
+  jupyterUsername: getJupyterHubUsername(store),
 });
 
 const mapDispatchToProps = {
